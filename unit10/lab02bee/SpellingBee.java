80f19048221c7dcbc21adfbaacb0b54e8fff4b8b
Kai Wallis <kona4kai@gmail.com>
2023-03-23 22:01:50 -0700

    Updated SpellingBee.java

--- unit10/lab02bee/SpellingBee.java
+++ unit10/lab02bee/SpellingBee.java
@@ -3,17 +3,44 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class SpellingBee {
 
     private char[] letters;
     private char mustUse;
 
-    // TODO construct me!
+    public SpellingBee (char[] letters, char mustUse) {
+        this.letters = letters;
+        this.mustUse = mustUse;
+    }
 
     public boolean checkWord(String word) {
-        // TODO implement me!
+        if(word.length() < 4 || word.indexOf(mustUse) == -1) {
+            return false;
+        }
+        char[] chars = word.toCharArray();
+        for(char character : chars) {
+            if(!characterInLetters(character)) {
+                return false;
+            }
+        }
         return true;
+    }
+
+    /**
+     * Checks if a character is one in letters[].
+     * @param character     The character to check.
+     * @return              Returns true if character is in letters[], false if not.
+     */
+    public boolean characterInLetters(char character) {
+        for(char letter : letters) {
+            if(character == letter) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -36,15 +63,40 @@ public class SpellingBee {
         return contents;
     }
 
+    private static void find(String findMe, String[] words) {
+        int start = 0;
+        int end = words.length-1;
+        int guess = (start + end)/2;
+        int check = words[guess].compareTo(findMe);
+        System.out.println("word is " + words[guess]);
+        System.out.println("check is " + check);
+        
+        while(check != 0) {
+            if(check < 0) {
+                start = guess+1;
+            } else if(check > 0) {
+                end = guess-1;
+            }
+            guess = (start + end)/2;
+            check = words[guess].compareTo(findMe);
+            System.out.println("word is " + words[guess]);
+        }
+        System.out.println("Location is " + guess);
+    }
+
     public static void main(String[] args) {
         String[] words = loadFile("words_dropped.txt").split("\n");
         System.out.println("Loaded " + words.length + " words");
-        // TODO solve me!
-        // SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
 
-        // TODO sort words!
+        SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
+        // SpellingBee bee = new SpellingBee("lvuoarf".toCharArray(), 'o');
 
-        // TODO what position in the sorted list is the word "search" ?
-
+        for(String word : words) {
+            if(bee.checkWord(word))
+                System.out.println(word);
+        }
+        
+        Arrays.sort(words);
+        find("potato", words);
     }
 }
